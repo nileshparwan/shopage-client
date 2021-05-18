043dc47d4cae1bae6010911acf8cca1fa2180673
Koshal Parwan <koshal.parwan@hangarww.com>
2021-05-19 00:03:21 +0400

    ADD global style

---
+++ styles/colors.ts
@@ -0,0 +1,49 @@
+import { css } from 'styled-components';
+
+export const colorStyles = css`
+:root {
+  --teal: #00b2a9;
+  --teal-transparent: #00b2a943;
+  --light-teal: #bbebe2;
+  --light-teal-transparent: #bbebe243;
+  --lightgrey: #f0f2f2;
+  --lightgrey-second: #dedede;
+  --indigo: #1e647d;
+  --dark-indigo: #1c5972;
+  --light-indigo: #7c96a4;
+  --black: #343434;
+  --bg-teal: #e5f6f6;
+  --bg-teal-transparent: #bbebe280;
+  --coral: #ff655b;
+  --yellow: #ffd406;
+  --indigo-hover: #6192a4;
+  --feedback-green: #55d66b;
+  --feedback-red: #ff0000;
+  --disabled-grey: #cfcfcf;
+  --white: #ffffff;
+  --coral-hover: #ff938c;
+  --teal-hover: #70d3ce;
+  --light-indigo-hover: #a3b5bf;
+  --facebook-blue: #1877f2;
+  --main: #00a6a6;
+  --light-cyan: #9decec;
+  --orange: #ff7100;
+  --dark-orange: #e56500;
+  --dark-grey: #696969;
+  --grey: #a8a8a8;
+  --dark-blue: #125d8c;
+  --disabled: #fac8ce;
+  --viking: #53c1c1;
+  --white-smoke: #f5f5f5;
+  --light-blue: #0080dc;
+  --grey-second: #787878;
+  --grey-third: #f9f9f9;
+  --grey-fourth: #4d4d4d;
+  --grey-fifth: #f4f4f4;
+  --dark-orange-second: #ff9605;
+  --dark-black: #000000;
+  --products-pure-lightblue: #89cac2;
+}
+`;
+
+export default colorStyles;
---
+++ styles/layout.ts
@@ -0,0 +1,15 @@
+import { css } from 'styled-components';
+
+export const layoutStyles = css`
+  body {
+    margin: 0;
+    padding: 0;
+  }
+  .container {
+    max-width: 1100px;
+    margin: 0 auto;
+    width: 100%;
+  }
+`;
+
+export default layoutStyles;
